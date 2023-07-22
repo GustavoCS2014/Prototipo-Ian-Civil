27c7a9b458552a5e9ffacaa5dbe515c276fffb49
César Zárate <cesarjzo@outlook.com>
2023-07-22 17:01:44 -0600

    Add LevelExitTrigger.cs

---
+++ Assets/Scripts/Units/LevelHandlers/LevelExitTrigger.cs
@@ -0,0 +1,17 @@
+﻿using Management;
+using UnityEngine;
+
+namespace Units.LevelHandlers
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class LevelExitTrigger : MonoBehaviour
+    {
+        [SerializeField] private Scene nextScene;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+                SceneManager.LoadScene(nextScene);
+        }
+    }
+}
