18013633208e0fc8a6d562b220993adeb91f7bb7
César Zárate <cesarjzo@outlook.com>
2023-07-22 21:49:48 -0600

    Add idle time to BossSettings.cs

--- Assets/Scripts/Entities/Boss/BossSettings.cs
+++ Assets/Scripts/Entities/Boss/BossSettings.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
+using Utilities;
 
 namespace Entities.Boss
 {
     [CreateAssetMenu(fileName = "Boss Settings", menuName = "Bosses/Boss Settings")]
     public class BossSettings : ScriptableObject
     {
-
+        [SerializeField] private Range idleTime;
+        public Range IdleTime => idleTime;
     }
 }
