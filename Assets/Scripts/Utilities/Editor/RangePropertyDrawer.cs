80e5e12eb37a7cae5fffdc32e3b6dae2903bac0e
César Zárate <cesarjzo@outlook.com>
2023-07-22 21:48:21 -0600

    Reduce RangePropertyDrawer.cs code and make Range.cs fields public

--- Assets/Scripts/Utilities/Editor/RangePropertyDrawer.cs
+++ Assets/Scripts/Utilities/Editor/RangePropertyDrawer.cs
@@ -1,44 +1,31 @@
-﻿using UnityEditor;
+﻿#if UNITY_EDITOR
+using UnityEditor;
 using UnityEngine;
 
 namespace Utilities.Editor
 {
     [CustomPropertyDrawer(typeof(Range))]
-    public class RangePropertyDrawer : PropertyDrawer
+    public sealed class RangePropertyDrawer : PropertyDrawer
     {
-        private const float SubLabelSpacing = 4;
-
-        public override void OnGUI(Rect pos, SerializedProperty prop, GUIContent label)
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            label = EditorGUI.BeginProperty(pos, label, prop);
-            Rect contentRect = EditorGUI.PrefixLabel(pos, GUIUtility.GetControlID(FocusType.Passive), label);
-            GUIContent[] labels = { new GUIContent("Min"), new GUIContent("Max") };
-            SerializedProperty[] properties = { prop.FindPropertyRelative("min"), prop.FindPropertyRelative("max") };
-            DrawMultiplePropertyFields(contentRect, labels, properties);
+            label = EditorGUI.BeginProperty(position, label, property);
+
+            SerializedProperty minProp = property.FindPropertyRelative("min");
+            SerializedProperty maxProp = property.FindPropertyRelative("max");
+
+            Rect contentRect = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+            EditorGUIUtility.labelWidth = 32f;
+
+            var minRect = new Rect(contentRect.x, contentRect.y, contentRect.width / 2 - 2, contentRect.height);
+            var maxRect = new Rect(contentRect.x + contentRect.width / 2 + 2, contentRect.y, contentRect.width / 2 - 2, contentRect.height);
+
+            EditorGUI.PropertyField(minRect, minProp, new GUIContent("Min"));
+            EditorGUI.PropertyField(maxRect, maxProp, new GUIContent("Max"));
 
             EditorGUI.EndProperty();
         }
-
-        private static void DrawMultiplePropertyFields(Rect pos, GUIContent[] subLabels, SerializedProperty[] props)
-        {
-            // backup gui settings
-            int originalIndentLevel = EditorGUI.indentLevel;
-            float originalLabelWidth = EditorGUIUtility.labelWidth;
-
-            // draw properties
-            int propsCount = props.Length;
-            float width = (pos.width - (propsCount - 1) * SubLabelSpacing) / propsCount;
-            Rect contentPos = new(pos.x, pos.y, width, pos.height);
-            EditorGUI.indentLevel = 0;
-            for (int i = 0; i < propsCount; i++)
-            {
-                EditorGUIUtility.labelWidth = EditorStyles.label.CalcSize(subLabels[i]).x;
-                EditorGUI.PropertyField(contentPos, props[i], subLabels[i]);
-                contentPos.x += width + SubLabelSpacing;
-            }
-
-            EditorGUIUtility.labelWidth = originalLabelWidth;
-            EditorGUI.indentLevel = originalIndentLevel;
-        }
     }
 }
+#endif
--- Assets/Scripts/Utilities/Range.cs
+++ Assets/Scripts/Utilities/Range.cs
@@ -6,25 +6,8 @@ namespace Utilities
     [Serializable]
     public struct Range
     {
-        // [HideInInspector]
-        [SerializeField]
-        private float min;
-
-        // [HideInInspector]
-        [SerializeField]
-        private float max;
-
-        public float Min
-        {
-            get => min;
-            set => min = value;
-        }
-
-        public float Max
-        {
-            get => max;
-            set => max = value;
-        }
+        public float min;
+        public float max;
 
         public Range(float min, float max)
         {
@@ -32,7 +15,7 @@ namespace Utilities
             this.max = max;
         }
 
-        public float RandomValue => UnityEngine.Random.Range(min, max);
+        public float Random => UnityEngine.Random.Range(min, max);
 
         public float Clamp(float value) => Mathf.Clamp(value, min, max);
 
