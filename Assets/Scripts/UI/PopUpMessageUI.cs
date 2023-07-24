6a7fc5d9ce7eedef1729a42e5cb73639ec1e8b00
César Zárate <cesarjzo@outlook.com>
2023-07-23 21:17:13 -0600

    Fix PopUpMessageUI.cs

--- Assets/Scripts/UI/PopUpMessageUI.cs
+++ Assets/Scripts/UI/PopUpMessageUI.cs
@@ -1,9 +1,19 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 namespace UI
 {
     public class PopUpMessageUI : MonoBehaviour
     {
+        [SerializeField] private string message;
+        [SerializeField] private TextMeshProUGUI text;
+
+        private void Start()
+        {
+            text.text = message;
+            Hide();
+        }
+
         public void Show()
         {
             gameObject.SetActive(true);
