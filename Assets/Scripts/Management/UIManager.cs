9157f16047c135c9dab73d860b27c0b64b1bfceb
César Zárate <cesarjzo@outlook.com>
2023-07-24 00:58:18 -0600

    Remove garbage from ButtonSelectionUI.cs

--- Assets/Scripts/Management/UIManager.cs
+++ Assets/Scripts/Management/UIManager.cs
@@ -1,5 +1,7 @@
-﻿using UnityEngine;
+﻿using Input;
+using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 namespace Management
 {
@@ -9,14 +11,37 @@ namespace Management
 
         public GameObject LastSelectedObject { get; set; }
 
-        private void Awake()
-        {
-            Instance = this;
-        }
-
         public void SetSelectedObject(GameObject selectedObject)
         {
             EventSystem.current.SetSelectedGameObject(selectedObject);
+        }
+
+        private void Awake()
+        {
+            Instance = this;
+            UIInput.Navigate += OnNavigate;
+            UIInput.PointPerformed += OnPointPerformed;
+        }
+
+        private void OnDestroy()
+        {
+            UIInput.Navigate -= OnNavigate;
+            UIInput.PointPerformed -= OnPointPerformed;
+        }
+
+
+        private void OnNavigate(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+                EventSystem.current.SetSelectedGameObject(LastSelectedObject);
+        }
+
+        private void OnPointPerformed(InputAction.CallbackContext context)
+        {
+            // todo: fix this
+            if (EventSystem.current.currentSelectedGameObject == LastSelectedObject)
+                return;
+            EventSystem.current.SetSelectedGameObject(null);
         }
     }
 }
--- Assets/Scripts/UI/ButtonSelectionUI.cs
+++ Assets/Scripts/UI/ButtonSelectionUI.cs
@@ -1,71 +1,37 @@
 ﻿using System.Collections;
-using Input;
 using Management;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.InputSystem;
 
 namespace UI
 {
     public sealed class ButtonSelectionUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler
     {
-        [SerializeField] private float verticalMoveAmount;
         [SerializeField] private float moveTime;
         [SerializeField] private float scaleAmount;
 
-        private Vector3 _startPosition;
         private Vector3 _startScale;
 
         private void Start()
         {
-            _startPosition = transform.position;
             _startScale = transform.localScale;
-
-            UIInput.Navigate += OnNavigate;
-            UIInput.PointPerformed += OnPointPerformed;
-        }
-
-        private void OnDestroy()
-        {
-            UIInput.Navigate -= OnNavigate;
-            UIInput.PointPerformed -= OnPointPerformed;
-        }
-
-        private void OnNavigate(InputAction.CallbackContext context)
-        {
-            if (context.performed)
-                EventSystem.current.SetSelectedGameObject(UIManager.Instance.LastSelectedObject);
-        }
-
-        private void OnPointPerformed(InputAction.CallbackContext context)
-        {
-            if (EventSystem.current.currentSelectedGameObject == UIManager.Instance.LastSelectedObject)
-                return;
-            EventSystem.current.SetSelectedGameObject(null);
         }
 
         private IEnumerator MoveCard(bool startingAnimation)
         {
-            Vector3 endPosition;
-            Vector3 endScale;
-
             for (var t = 0f; t < moveTime; t += Time.deltaTime)
             {
+                Vector3 endScale;
                 if (startingAnimation)
                 {
-                    endPosition = _startPosition + Vector3.up * verticalMoveAmount;
                     endScale = _startScale * scaleAmount;
                 }
                 else
                 {
-                    endPosition = _startPosition;
                     endScale = _startScale;
                 }
-
-                Vector3 lerpPosition = Vector3.Lerp(transform.position, endPosition, t / moveTime);
                 Vector3 lerpScale = Vector3.Lerp(transform.localScale, endScale, t / moveTime);
 
-                transform.position = lerpPosition;
                 transform.localScale = lerpScale;
 
                 yield return null;
