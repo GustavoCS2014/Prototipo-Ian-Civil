f9f6f7faf74532f92cfd979b9e46d7411feee832
César Zárate <cesarjzo@outlook.com>
2023-07-25 12:41:29 -0600

    Rename to TweenScaleUI.cs and make events for initial and final tweens

---
+++ Assets/Scripts/UI/TweenScaleUI.cs
@@ -0,0 +1,50 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace UI
+{
+    public class TweenScaleUI : MonoBehaviour
+    {
+        [SerializeField] private Vector3 initialScale;
+        [SerializeField] private Vector3 finalScale;
+
+        [SerializeField] private Ease ease;
+        [SerializeField] private float tweenTime;
+
+        [SerializeField] private UnityEvent onInitialScaleComplete;
+        [SerializeField] private UnityEvent onFinalScaleComplete;
+
+        private RectTransform _rectTransform;
+
+        private void Awake()
+        {
+            _rectTransform = GetComponent<RectTransform>();
+        }
+
+        private void Start()
+        {
+            _rectTransform.localScale = initialScale;
+        }
+
+        public void DoInitialScale()
+        {
+            _rectTransform.DOScale(initialScale, tweenTime).SetEase(ease).OnComplete(() =>  onInitialScaleComplete?.Invoke());
+        }
+
+        public void DoInitialScaleAfterDelay(float delay)
+        {
+            _rectTransform.DOScale(initialScale, tweenTime).SetEase(ease).SetDelay(delay).OnComplete(() => onInitialScaleComplete?.Invoke());
+        }
+
+        public void DoFinalScale()
+        {
+            _rectTransform.DOScale(finalScale, tweenTime).SetEase(ease).OnComplete(() => onFinalScaleComplete?.Invoke());
+        }
+
+        public void DoFinalScaleAfterDelay(float delay)
+        {
+            _rectTransform.DOScale(finalScale, tweenTime).SetEase(ease).SetDelay(delay).OnComplete(() => onFinalScaleComplete?.Invoke());
+        }
+    }
+}
