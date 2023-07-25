82915aa278aa03cb0187d4538ac7a56ad6756116
César Zárate <cesarjzo@outlook.com>
2023-07-24 21:01:16 -0600

    Use library on PopUpMessageUI.cs

--- Assets/Scripts/UI/PopUpMessageUI.cs
+++ Assets/Scripts/UI/PopUpMessageUI.cs
@@ -1,27 +1,40 @@
-﻿using TMPro;
+﻿using DG.Tweening;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace UI
 {
     public class PopUpMessageUI : MonoBehaviour
     {
+        [SerializeField] private Ease ease;
+        [SerializeField] private float tweenTime;
         [SerializeField] private string message;
         [SerializeField] private TextMeshProUGUI text;
+        [SerializeField] private UnityEvent onHide;
+
+        private RectTransform _rectTransform;
+
+        private void Awake()
+        {
+            _rectTransform = GetComponent<RectTransform>();
+        }
 
         private void Start()
         {
             text.text = message;
-            Hide();
+
+            _rectTransform.localScale = new Vector3(1f, 0f, 1f);
         }
 
         public void Show()
         {
-            gameObject.SetActive(true);
+            _rectTransform.DOScaleY(1f, tweenTime).SetEase(ease).onComplete += Hide;
         }
 
         public void Hide()
         {
-            gameObject.SetActive(false);
+            _rectTransform.DOScaleY(0f, tweenTime).SetEase(ease).SetDelay(2f).onComplete += () => onHide?.Invoke();
         }
     }
 }
