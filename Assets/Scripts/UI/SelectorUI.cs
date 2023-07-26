73374123f0e35628dc9e27725db7f899983fedc0
César Zárate <cesarjzo@outlook.com>
2023-07-26 14:05:30 -0600

    Add transitions to SelectorUI.cs and change visuals of MainMenu.unity scene canvas

--- Assets/Scripts/UI/SelectorUI.cs
+++ Assets/Scripts/UI/SelectorUI.cs
@@ -6,16 +6,23 @@ namespace UI
     public class SelectorUI : MonoBehaviour
     {
         [SerializeField] private Vector2 offset;
+        [SerializeField] private float baseScale;
 
-        [SerializeField] private float speed;
+        [SerializeField] private float transitionSpeed;
+        [SerializeField] private float scaleSpeed;
         [SerializeField] private float amplitude;
 
         private RectTransform _rectTransform;
+        private Vector2 _targetSizeDelta;
+        private Vector3 _targetPosition;
 
         private void Awake()
         {
             UIManager.SelectedChanged += OnSelectedChanged;
             _rectTransform = GetComponent<RectTransform>();
+
+            _targetSizeDelta = _rectTransform.sizeDelta;
+            _targetPosition = _rectTransform.position;
         }
 
         private void OnDestroy()
@@ -26,17 +33,18 @@ namespace UI
         private void OnSelectedChanged(GameObject selectedObject)
         {
             if (!selectedObject) return;
-
             var selectedTransform = selectedObject.GetComponent<RectTransform>();
 
             if (!selectedTransform) return;
-            _rectTransform.position = selectedTransform.position;
-            _rectTransform.sizeDelta = selectedTransform.sizeDelta + offset;
+            _targetPosition = selectedTransform.position;
+            _targetSizeDelta = selectedTransform.sizeDelta + offset;
         }
 
         private void Update()
         {
-            _rectTransform.localScale = Vector3.one * (1f + Mathf.Sin(Time.time * speed) * amplitude);
+            _rectTransform.position = Vector3.Lerp(_rectTransform.position, _targetPosition, transitionSpeed * Time.deltaTime);
+            _rectTransform.sizeDelta = Vector2.Lerp(_rectTransform.sizeDelta, _targetSizeDelta, transitionSpeed * Time.deltaTime);
+            _rectTransform.localScale = Vector3.one * (baseScale + Mathf.Sin(Time.time * scaleSpeed) * amplitude);
         }
     }
 }
