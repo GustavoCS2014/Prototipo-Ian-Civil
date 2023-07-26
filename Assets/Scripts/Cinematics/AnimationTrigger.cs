e49c15c3e756d989ca2a078789dc3c5fdb1c15ce
César Zárate <cesarjzo@outlook.com>
2023-07-26 12:28:10 -0600

    Add AnimationEvent.cs and AnimationTrigger.cs scripts

---
+++ Assets/Scripts/Cinematics/AnimationTrigger.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Cinematics
+{
+    public class AnimationTrigger : MonoBehaviour
+    {
+        [SerializeField] private string startTrigger;
+        [SerializeField] private UnityEvent onStart;
+
+        [SerializeField] private string endTrigger;
+        [SerializeField] private UnityEvent onEnd;
+
+        private void Awake()
+        {
+            AnimationEvent.Started += OnStarted;
+            AnimationEvent.Ended += OnEnded;
+        }
+
+        private void OnDestroy()
+        {
+            AnimationEvent.Started -= OnStarted;
+            AnimationEvent.Ended -= OnEnded;
+        }
+
+        private void OnStarted(string trigger)
+        {
+            ReadOnlySpan<char> triggerSpan = trigger.AsSpan().Trim();
+            ReadOnlySpan<char> startTriggerSpan = startTrigger.AsSpan().Trim();
+
+            if (triggerSpan.IsEmpty || startTriggerSpan.IsEmpty)
+                return;
+
+            if (triggerSpan.SequenceEqual(startTriggerSpan))
+                onStart?.Invoke();
+        }
+
+        private void OnEnded(string trigger)
+        {
+            ReadOnlySpan<char> triggerSpan = trigger.AsSpan().Trim();
+            ReadOnlySpan<char> endTriggerSpan = endTrigger.AsSpan().Trim();
+
+            if (triggerSpan.IsEmpty || endTriggerSpan.IsEmpty)
+                return;
+
+            if (triggerSpan.SequenceEqual(endTriggerSpan))
+                onEnd?.Invoke();
+        }
+    }
+}
--- Assets/Scripts/Units/LevelHandlers/LoadSceneTrigger.cs
+++ Assets/Scripts/Units/LevelHandlers/LoadSceneTrigger.cs
@@ -6,8 +6,6 @@ namespace Units.LevelHandlers
 {
     public sealed class LoadSceneTrigger : MonoBehaviour
     {
-        [SerializeField] private GameState loadSceneIfState;
-
         [SerializeField] private GameScene nextScene;
 
         private void OnTriggerEnter2D(Collider2D other)
@@ -19,24 +17,6 @@ namespace Units.LevelHandlers
         public void LoadNextScene()
         {
             SceneManager.LoadScene(nextScene);
-        }
-
-        private void Start()
-        {
-            if (loadSceneIfState != 0)
-                GameManager.StateChanged += OnGameStateChange;
-        }
-
-        private void OnGameStateChange(GameState state)
-        {
-            if (state.HasFlag(loadSceneIfState))
-                LoadNextScene();
-        }
-
-        private void OnDestroy()
-        {
-            if (loadSceneIfState != 0)
-                GameManager.StateChanged -= OnGameStateChange;
         }
     }
 }
