6e3f912c954509083c3a40b2c669d6f48b69fcca
César Zárate <cesarjzo@outlook.com>
2023-07-27 15:15:59 -0600

    Add Attributes folder with ReadOnlyAttribute.cs and TagSelectorAttribute.cs

---
+++ Assets/Scripts/Attributes/ReadOnlyAttribute.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Attributes
+{
+    public class ReadOnlyAttribute : PropertyAttribute { }
+
+#if UNITY_EDITOR
+    [UnityEditor.CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    public class ReadOnlyDrawer : UnityEditor.PropertyDrawer
+    {
+        public override void OnGUI(Rect position, UnityEditor.SerializedProperty property, GUIContent label)
+        {
+            bool wasEnabled = GUI.enabled;
+            GUI.enabled = false;
+            UnityEditor.EditorGUI.PropertyField(position, property, label);
+            GUI.enabled = wasEnabled;
+        }
+    }
+#endif
+}
