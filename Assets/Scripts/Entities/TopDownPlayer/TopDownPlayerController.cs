b34e43279b62e5276a082723d72dc7d24c923fd4
César Zárate <cesarjzo@outlook.com>
2023-10-07 23:02:07 -0600

    Add TopDownPlayerController.cs

---
+++ Assets/Scripts/Entities/TopDownPlayer/TopDownPlayerController.cs
@@ -0,0 +1,58 @@
+﻿using Input;
+using UnityEngine;
+
+namespace Entities.TopDownPlayer
+{
+    public sealed class TopDownPlayerController : MonoBehaviour
+    {
+        [SerializeField] private TopDownPlayerSettings settings;
+
+        private void Update()
+        {
+            HandleMovement();
+        }
+
+        private void HandleMovement()
+        {
+            Vector2 moveDirection = GameplayInput.MoveDirection;
+            float moveDistance = settings.Speed * Time.deltaTime;
+
+            bool canMove = !CircleCast(moveDirection);
+
+            if (!canMove)
+            {
+                var moveDirectionX = new Vector2(moveDirection.x, 0f);
+                const float perpendicularDeadZone = 0.5f;
+                canMove = Mathf.Abs(moveDirection.x) > perpendicularDeadZone && !CircleCast(moveDirectionX);
+
+                if (canMove)
+                {
+                    moveDirection = moveDirectionX;
+                }
+                else
+                {
+                    var moveDirectionY = new Vector2(0f, moveDirection.y);
+                    canMove = Mathf.Abs(moveDirection.y) > perpendicularDeadZone && !CircleCast(moveDirectionY);
+
+                    if (canMove)
+                    {
+                        moveDirection = moveDirectionY;
+                    }
+                }
+            }
+
+            if (canMove)
+                transform.Translate(moveDirection * moveDistance);
+
+            return;
+
+            RaycastHit2D CircleCast(Vector2 direction) => Physics2D.CircleCast(
+                origin: transform.position,
+                radius: settings.Radius,
+                direction: direction,
+                distance: moveDistance,
+                layerMask: settings.WallLayer
+            );
+        }
+    }
+}
---
+++ Assets/Scripts/Entities/TopDownPlayer/TopDownPlayerSettings.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Entities.TopDownPlayer
+{
+    [CreateAssetMenu(fileName = "Top Down Player Settings", menuName = "Player/Top Down Settings")]
+    public sealed class TopDownPlayerSettings : ScriptableObject
+    {
+        [SerializeField] private float speed;
+        public float Speed => speed;
+
+        [SerializeField] private float radius;
+        public float Radius => radius;
+
+        [SerializeField] private LayerMask wallLayer;
+        public LayerMask WallLayer => wallLayer;
+    }
+}
